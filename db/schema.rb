f4a5ccdc5fd718c27688a617400e0aafe137affe
carlozaul25 <carloz18rivera@live.com>
2018-07-02 12:33:42 -0400

    published or draft

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-j# This file is auto-generated from the current state of the database. Instead
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -10,7 +10,7 @@ j# This file is auto-generated from the current state of the database. Instead
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_29_010240) do
+ActiveRecord::Schema.define(version: 2018_06_29_203133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_06_29_010240) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
   end
 
   create_table "portafolios", force: :cascade do |t|
