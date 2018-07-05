b129e2a4092f8b68d1d7df52b7e682c1b010b186
carlozaul25 <carloz18rivera@live.com>
2018-07-05 16:01:51 -0400

    guess user welcome

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,17 @@
 class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
-
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
 		devise_parameter_sanitizer.permit(:acount_update, keys: [:name])
 	end
 
 	before_action :set_source 
-
 	def set_source
 		session[:source] = params[:q] if params[:q]
+	end
+
+	def current_user 
+		super ||  OpenStruct.new(name: "Guest User", first_name: "Guest", last_name: "User", email: 'guest@email.com')
 	end
 
 end
